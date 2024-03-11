52664b8a5ea0870977b985c029ca1b717af28708
Viktor Ciolkowski <wikcioo@gmail.com>
2024-03-11 08:38:36 +0100

    Add logic operations example firmware

---
+++ firmware/logic_ops/main.c
@@ -0,0 +1,43 @@
+#include "switch.h"
+#include "led_array.h"
+
+int main()
+{
+    switch_init();
+    led_array_init();
+
+    while (1)
+    {
+        b8 s7 = switch_get(SWITCH7);
+        b8 s8 = switch_get(SWITCH8);
+
+        led_array_clear();
+
+        if (s7 & s8)
+        {
+            led_array_set(LED8, LED_HIGH);
+        }
+        if (s7 | s8)
+        {
+            led_array_set(LED7, LED_HIGH);
+        }
+        if (s7 ^ s8)
+        {
+            led_array_set(LED6, LED_HIGH);
+        }
+        if (!(s7 & s8))
+        {
+            led_array_set(LED5, LED_HIGH);
+        }
+        if (!(s7 | s8))
+        {
+            led_array_set(LED4, LED_HIGH);
+        }
+        if (!(s7 ^ s8))
+        {
+            led_array_set(LED3, LED_HIGH);
+        }
+    }
+
+    return 0;
+}
